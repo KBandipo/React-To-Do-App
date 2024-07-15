84ad7cc6c67914dba80c3d71d17f9cd1fdb84ea3
KBandipo~ <keneebandipo@gmail.com>
2024-07-15 22:51:33 +0100

    Create Header Content

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { MdPostAdd, MdMessage } from "react-icons/md";
+import classes from "./Header.module.css";
+function Header() {
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.logo}>
+        <MdMessage />
+        To-Do App
+      </h1>
+      <p>
+        <button className={classes.button}>
+          <MdPostAdd size={18} />
+          Post New Task
+        </button>
+      </p>
+    </header>
+  );
+}
+
+export default Header;
