b374001334cae29ab36d829ee153ee7377c476cb
KBandipo~ <keneebandipo@gmail.com>
2024-07-16 18:37:38 +0100

    Create content of NewToDo.jsx

---
+++ src/components/NewToDo.jsx
@@ -0,0 +1,22 @@
+import classes from "./NewToDo.module.css";
+
+function NewToDo() {
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} />
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+      <p className={classes.actions}>
+        <button type="button">Cancel</button>
+        <button>Submit</button>
+      </p>
+    </form>
+  );
+}
+
+export default NewToDo;
