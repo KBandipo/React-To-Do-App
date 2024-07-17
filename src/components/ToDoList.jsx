3ce7fb86c68e892749f4f8241169273156c9b421
KBandipo~ <keneebandipo@gmail.com>
2024-07-18 00:25:28 +0100

    Display post of ToDoList conditionally

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import classes from "./ToDoList.module.css";
+import PostToDo from "./PostToDo";
+import NewToDo from "./NewToDo";
+import Modal from "./Modal";
 
-function ToDoList() {
-  return <div></div>;
+function ToDoList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPost) => [postData, ...existingPost]);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewToDo onClose={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 ? (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <PostToDo key={post.day} day={post.day} task={post.task} />
+          ))}
+        </ul>
+      ) : (
+        <div style={{ textAlign: "center", color: "#fff" }}>
+          <h2>There are no Tasks yet</h2>
+          <p>Post a New Task!</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ToDoList;
