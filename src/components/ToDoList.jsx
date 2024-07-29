5357a8b6f2c721740dbf53109ff92127207b6410
KBandipo~ <keneebandipo@gmail.com>
2024-07-29 11:33:55 +0100

    Use useEffect hook to fetch data from backend

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./ToDoList.module.css";
 import PostToDo from "./PostToDo";
 import NewToDo from "./NewToDo";
@@ -6,6 +6,18 @@ import Modal from "./Modal";
 
 function ToDoList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -25,16 +37,24 @@ function ToDoList({ isPosting, onStopPosting }) {
           <NewToDo onClose={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 ? (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <PostToDo key={post.day} day={post.day} task={post.task} />
           ))}
         </ul>
-      ) : (
+      )}
+
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "#fff" }}>
           <h2>There are no Tasks yet</h2>
           <p>Post a New Task!</p>
+        </div>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading To-do tasks...</p>
         </div>
       )}
     </>
