e244653ee53fea988edc7764a2e4cb4735da77b3
KBandipo~ <keneebandipo@gmail.com>
2024-07-30 16:27:46 +0100

    Change direction of App.jsx to router folder

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Header from "./components/Header";
-import ToDoList from "./components/ToDoList";
+import Header from "../components/Header";
+import ToDoList from "../components/ToDoList";
 
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
