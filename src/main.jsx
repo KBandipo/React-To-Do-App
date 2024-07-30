64740c20b8f6f9d5263bd4744b4e92be55a0016f
KBandipo~ <keneebandipo@gmail.com>
2024-07-30 13:40:27 +0100

    Nest children route to RootLayout

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,16 @@ import App from "./App.jsx";
 import NewToDo from "./components/NewToDo.jsx";
 import RootLayout from "./routers/RootLayout.jsx";
 
-const router = createBrowserRouter([{ path: "/", element: <RootLayout /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-todo", element: <NewToDo /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
